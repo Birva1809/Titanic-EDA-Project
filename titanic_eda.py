0c2083a762165b0440b88edbd655b20bc250b45c
Birva <birva1809@gmail.com>
2025-04-14 21:56:05 +0530

    Updated Python File
    Modified Visualizations

--- titanic_eda.py
+++ titanic_eda.py
@@ -24,18 +24,18 @@ first_class = df [df ["Pclass"] == 1]
 print("First Class Passengers: \n", first_class.head()) 
 
 # Bar Chart: Survival rate by class 
-# survival_by_class = df.groupby ("Pclass") ["Survived"].mean() 
-# survival_by_class.plot(kind="bar", color="skyblue") 
-# plt.title("Survival Rate by class") 
-# plt.ylabel("Survival Rate") 
-# plt.show()
+survival_by_class = df.groupby ("Pclass") ["Survived"].mean() 
+survival_by_class.plot(kind="bar", color="skyblue") 
+plt.title("Survival Rate by class") 
+plt.ylabel("Survival Rate") 
+plt.show()
 
 #Histogram: Age distribution 
-# sns.histplot(df ["Age"], kde = True, bins=20, color="purple") 
-# plt.title("Age Distribution") 
-# plt.xlabel("Age") 
-# plt.ylabel("Frequency") 
-# plt.show()
+sns.histplot(df ["Age"], kde = True, bins=20, color="purple") 
+plt.title("Age Distribution") 
+plt.xlabel("Age") 
+plt.ylabel("Frequency") 
+plt.show()
 
 # Scatter Plot: Age vs Fare 
 plt.scatter(df ["Age"], df ["Fare"], alpha=0.5, color="green") 
